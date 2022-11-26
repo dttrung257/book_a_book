c5ee491c8302617b3a042d00285e6037f230e4af
hungtran_20021371 <duyhungtran716@gmail.com>
2022-11-26 22:18:15 +0700

    seach by book name

---
+++ frontend/src/store/searchSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState={
+    name:"",
+};
+
+const searchSlice = createSlice({
+    name: "filter",
+    initialState,
+    reducers: {
+        setNameSearch(state,action) {
+            state.name = action.payload.name;
+        }
+    }
+});
+
+export const searchActions=searchSlice.actions;
+
+export default searchSlice.reducer;
